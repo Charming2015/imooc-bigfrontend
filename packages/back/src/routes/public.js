276ec56376c0c4c5f0cb6ce0accab6d4a4fdeaf8
陈汉明 <chenhanming@bytedance.com>
2020-10-23 15:01:26 +0800

    feat: test webhooks

--- packages/back/src/routes/public.js
+++ packages/back/src/routes/public.js
@@ -4,5 +4,15 @@ const router = new Router();
 
 router.prefix('/public');
 router.get('/getCaptcha', publicController.getCaptcha);
+router.post('/gitWebHooks', async (ctx) => {
+  console.log(ctx.request);
+  console.log('=====================');
+  console.log(ctx.request.body);
+  console.log('=====================');
+  ctx.body = {
+    code: 200,
+    msg: 'ok',
+  };
+});
 
 export default router;
